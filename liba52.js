fa71e564660158b3522e520fc9e1cfae54c844ab
jgn <gorinje@gmail.com>
2023-11-24 16:34:51 +0100

    Add a52 tests

---
+++ liba52.js
@@ -0,0 +1,29 @@
+describe('#libjxl', () => {
+	it('should decode Motivation.ac3 to wav with worker', (done) => {
+		create_test('audio',
+			'universal-audio_1',
+			"solver_1",
+			"liba52_1",
+			"https://bevara.ddns.net/test-signals/ac3/Motivation.ac3",
+			"https://bevara.ddns.net/test-signals/out/ac3/Motivation.wav",
+			done,
+			"wav",
+			false,
+			false
+		);
+	}).timeout(5000);
+
+	it('should decode Motivation.ac3 to wav without worker', (done) => {
+		create_test('audio',
+			'universal-audio_1',
+			"solver_1",
+			"liba52_1",
+			"https://bevara.ddns.net/test-signals/ac3/Motivation.ac3",
+			"https://bevara.ddns.net/test-signals/out/ac3/Motivation.wav",
+			done,
+			"wav",
+			false,
+			true
+		);
+	}).timeout(5000);
+});
