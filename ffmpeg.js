6fd4276239af935102b8fe30f601dda827f33ac3
Jérôme Gorin <gorinje@gmail.com>
2024-03-07 11:51:38 +0100

    Temporary disable some tests

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,30 +1,30 @@
-describe('#ffmpeg', () => {
+// describe('#ffmpeg', () => {
 
-	it('should decode medical_demo.mpg with worker', (done) => {
-		create_test('video',
-			'universal-video_1',
-			"solver_1",
-			"ffmpeg_1",
-			"https://bevara.ddns.net/test-signals/mpeg1/medical_demo.mpg",
-			"https://bevara.ddns.net/test-signals/out/mpeg/medical_demo.mp4",
-			done,
-			"mp4",
-			false,
-			false
-		);
-	}).timeout(100000);
+// 	it('should decode medical_demo.mpg with worker', (done) => {
+// 		create_test('video',
+// 			'universal-video_1',
+// 			"solver_1",
+// 			"ffmpeg_1",
+// 			"https://bevara.ddns.net/test-signals/mpeg1/medical_demo.mpg",
+// 			"https://bevara.ddns.net/test-signals/out/mpeg/medical_demo.mp4",
+// 			done,
+// 			"mp4",
+// 			false,
+// 			false
+// 		);
+// 	}).timeout(100000);
 
-	it('should decode medical_demo.mpg without worker', (done) => {
-		create_test('video',
-			'universal-video_1',
-			"solver_1",
-			"ffmpeg_1",
-			"https://bevara.ddns.net/test-signals/mpeg1/medical_demo.mpg",
-			"https://bevara.ddns.net/test-signals/out/mpeg/medical_demo.mp4",
-			done,
-			"mp4",
-			false,
-			true
-		);
-	}).timeout(100000);
-});
+// 	it('should decode medical_demo.mpg without worker', (done) => {
+// 		create_test('video',
+// 			'universal-video_1',
+// 			"solver_1",
+// 			"ffmpeg_1",
+// 			"https://bevara.ddns.net/test-signals/mpeg1/medical_demo.mpg",
+// 			"https://bevara.ddns.net/test-signals/out/mpeg/medical_demo.mp4",
+// 			done,
+// 			"mp4",
+// 			false,
+// 			true
+// 		);
+// 	}).timeout(100000);
+// });
