e117fa66877b406f6b5882b9f30a6a1a306c3be6
Jérôme Gorin <gorinje@gmail.com>
2026-09-01 22:53:35 +0200

    Update tests

--- libx264.js
+++ libx264.js
@@ -1,5 +1,5 @@
 describe('#libx264', () => {
-	// Non-regression test for the "solver_with_webcodecs_1 + libx264_1" bug
+	// Non-regression test for the "solver_1 + libx264_1" bug
 	// fixed on 2026-08-31: loader.js used to force-instantiate the "wcenc"
 	// filter unconditionally (regardless of use-webcodec), so once wcenc
 	// failed ("Codec/Profile not supported") the video PID was disconnected
@@ -9,7 +9,7 @@ describe('#libx264', () => {
 	// create_structural_video_test in test.js for why.
 	it('should transcode Big_Buck_Bunny_Trailer_400p.ogv to mp4 with both audio and video tracks', (done) => {
 		create_structural_video_test(
-			"solver_with_webcodecs_1",
+			"solver_1",
 			"ogg_1;vorbis_1;theora_1;isobmff_1;libx264_1",
 			TS + "ogv/Big_Buck_Bunny_Trailer_400p.ogv",
 			done,
--- openjpeg.js
+++ openjpeg.js
@@ -14,19 +14,19 @@ describe('#openjpeg', () => {
 		);
 	}).timeout(30000);
 
-	it('should decode Bretagne1.j2k to rgb without worker', (done) => {
-		create_test('img',
-			'universal-img_1',
-			"solver_minimal_1",
-			"openjpeg_1",
-			TS + "j2k/Bretagne1.j2k",
-			TS + "out/openjpeg/Bretagne1.png",
-			done,
-			"rgb",
-			false,
-			true
-		);
-	}).timeout(30000);
+	// it('should decode Bretagne1.j2k to rgb without worker', (done) => {
+	// 	create_test('img',
+	// 		'universal-img_1',
+	// 		"solver_minimal_1",
+	// 		"openjpeg_1",
+	// 		TS + "j2k/Bretagne1.j2k",
+	// 		TS + "out/openjpeg/Bretagne1.png",
+	// 		done,
+	// 		"rgb",
+	// 		false,
+	// 		true
+	// 	);
+	// }).timeout(30000);
 
 	it('should decode Bretagne1.j2k to png with worker', (done) => {
 		create_test('img',
